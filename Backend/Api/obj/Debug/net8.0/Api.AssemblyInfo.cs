ea1a765d7401acadad4fdf698b0604a51c1c8109
xcl2406 <lehieu122004@gmail.com>
2026-05-26 18:31:08 +0700

    delete trash file

--- Backend/Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ Backend/Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1dc04476ec7bb03e2d14e2be32f99c071f15a065")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+534d8d00c5d563055adeb9b02c6e6332a4af84fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Backend/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Backend/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1dc04476ec7bb03e2d14e2be32f99c071f15a065")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+534d8d00c5d563055adeb9b02c6e6332a4af84fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
